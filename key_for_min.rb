47fcf0b38457941e3b2ffbed8c9d0e3cd714d67a
Jacob Menashi <jbmenashi@gmail.com>
2018-10-03 16:45:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
   else
-  name_hash.each do |item, cost|
-    return item
+  name_hash.collect do |item, cost|
+    lowest_cost = 100000
+    if name_hash[:item] < lowest_cost
+      lowest_cost = name_hash[:item]
+    end
+    lowest_cost
   end
     
     
