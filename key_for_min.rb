944881feb7f7d58e081a751ffa32fa6f9682ed66
Jacob Menashi <jbmenashi@gmail.com>
2018-10-03 16:49:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
   else
-  name_hash.each do |item, cost|
+    min_value = nil
+    min_name = nil
+    name_hash.each do |item, cost|
     return item
   end
     
